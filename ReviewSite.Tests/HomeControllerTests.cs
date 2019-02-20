5d7d0ae43219b993aaf054a5a600259c9f693290
Alex Albright <alexanderjalbright@gmail.com>
2019-02-20 13:47:17 -0500

    Initial Commit

---
+++ ReviewSite.Tests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ReviewSite.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
