1dee916631b765d8cf000e7bcc65bc0fc0d1ff46
khani <khani@DAVITI>
2019-02-22 09:40:50 -0500

    test

--- ReviewSite/Program.cs
+++ ReviewSite/Program.cs
@@ -19,6 +19,6 @@ namespace ReviewSite
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); //Comment1
     }
 }
