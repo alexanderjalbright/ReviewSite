65dfa9a6546e71a2797d5459ead35ee9a32b5447
Artie Negron <anegroncsu@gmail.com>
2019-02-21 23:53:01 -0500

    Started basic layout for Review Index.

--- ReviewSite/Repositories/ReviewRepository.cs
+++ ReviewSite/Repositories/ReviewRepository.cs
@@ -14,9 +14,9 @@ namespace ReviewSite.Repositories
         {
             reviewList = new List<Review>()
             {
-                new Review("Machine Learning", 1, "Algorithms", "./images/MachineLearning.jpg", "Learn to use machine learning to your advantage.", "Learn Machine learning.", new List<string>() { "Its free!", "You get what you pay for and it's free.", "The instructors are racist." }),
-                new Review("Python For Everybody", 2, "Python", "./images/PythonForEveryone.jpg", "It really is for everybody!", "Er'body can!", new List<string>() { "Its so close!", "The carpets smell like urine.", "The coffee has a peculiar taste." }),
-                new Review("WWKD", 3, "C#", "./images/WWKD.jpg", "It definitely not for everybody!", "Some people can!", new List<string>() { "The instructors love you like their own family!", "If I could experience Groundhog Day here, I'd still never get enough.", "This place attracts weirdos." })
+                new Review("Machine Learning", 1, "Algorithms", "/Images/machinelearning.jpg", "Learn to use machine learning to your advantage.", "Learn Machine learning.", new List<string>() { "Its free!", "You get what you pay for and it's free.", "The instructors are racist." }),
+                new Review("Python For Everybody", 2, "Python", "/Images/pythonforeveryone.jpg", "It really is for everybody!", "Er'body can!", new List<string>() { "Its so close!", "The carpets smell like urine.", "The coffee has a peculiar taste." }),
+                new Review("WWKD?", 3, "What would Kyle do?", "/Images/wwkd.jpg", "It's definitely not for everybody!", "Some people can!", new List<string>() { "The instructors love you like their own family!", "If I could experience Groundhog Day here, I'd still never get enough.", "This place attracts weirdos." })
             };
         } 
 
