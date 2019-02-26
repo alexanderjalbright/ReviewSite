97fdd411c491adc82f4227a89cea496cd2d24e57
Alex Albright <alexanderjalbright@gmail.com>
2019-02-26 16:02:46 -0500

    setup seed data, refractored reviewRepo and ReviewController, added entity services

--- ReviewSite/Repositories/ReviewRepository.cs
+++ ReviewSite/Repositories/ReviewRepository.cs
@@ -10,161 +10,28 @@ namespace ReviewSite.Repositories
     {
         List<Review> reviewList;
 
-        public ReviewRepository()
-        {
-            reviewList = new List<Review>()
-            {
-                new Review()
-                {
-                    Title = "Machine Learning",
-                    Id = 1,
-                    Category = "Algorithms",
-                    ImageURL = "/Images/machinelearning.jpg",
-                    Overview = "Learn to use machine learning to your advantage.",
-                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                    TagList = new List<string>() {"C#", "Algorithms", "Machine Learning"},
-                    UserOpinionList =  new List<UserReview>()
-                    {
-                        new UserReview()
-                        {
-                            UserName = "MoocJunkie",
-                            Comment = "Its free!",
-                            Rating = 3.3M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "SwagYoloFleekDabs",
-                            Comment = "You get what you pay for and it's free.",
-                            Rating = 4.1M
-                        },
-                        new UserReview(){
-                            UserName = "xXx_DragonSlayer_xXx",
-                            Comment = "The instructors are racist.",
-                            Rating = 3.9M
-                        }
+        ReviewContext db;
 
-                    }
-                },
-                new Review()
-                {
-                    Title = "Python For Everybody",
-                    Id = 2,
-                    Category = "Python",
-                    ImageURL = "/Images/pythonforeveryone.jpg",
-                    Overview = "It really is for everybody!",
-                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                    TagList = new List<string>() {"Python", "Algorithms", "Coding"},
-                    UserOpinionList = new List<UserReview>()
-                    {
-                        new UserReview()
-                        {
-                            UserName = "JoeLovesFishing_USA",
-                            Comment = "The instructors love you like their own family!",
-                            Rating = 4.9M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "elDiablo1992",
-                            Comment = "If I could experience Groundhog Day here, I'd still never get enough.",
-                            Rating = 5.0M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "W3CanC0d31T",
-                            Comment = "This place attracts weirdos.",
-                            Rating = 4.1M
-                        }
-                    }
-                },
-                new Review()
-                {
-                    Title = "WWKD?",
-                    Id = 3,
-                    Category = "What would Kyle do?",
-                    ImageURL = "/Images/wwkd.jpg",
-                    Overview = "It's definitely not for everybody!",
-                    Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                    TagList = new List<string>() {"C#", "ASP.NET", "MVC"},
-                    UserOpinionList = new List<UserReview>()
-                    {
-                        new UserReview()
-                        {
-                            UserName = "anegronjr",
-                            Comment = "Its so close!",
-                            Rating = 2.2M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "alexanderjalbright",
-                            Comment = "The carpets smell like urine.",
-                            Rating = 1.7M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "khanishvili",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 2.2M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "jen3ten",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 2.3M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "MaryMcGeary",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 2.0M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "mroyski",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 1.5M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "aldenmolina",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 3.0M
-                        },
-                        new UserReview()
-                        {
-                            UserName = "JoeScheiman",
-                            Comment = "The coffee has a peculiar taste.",
-                            Rating = 3.0M
-                        }
-                    }
-                }
-            };
+        public ReviewRepository(ReviewContext db)
+        {
+            this.db = db;
         } 
 
         public int Count()
         {
-            int count = reviewList.Count;
+            int count = db.Reviews.Count();
 
             return count;
         }
 
-        public List<Review> GetAll()
+        public IEnumerable<Review> GetAll()
         {
-            return reviewList;
+            return db.Reviews.ToList();
         }
 
         public Review GetById(int id)
-        {
-            Review review = new Review();
-
-            foreach (Review eachOne in reviewList)
-            {
-                if(eachOne.Id == id)
-                {
-                    review = eachOne;
-                }
-            }
-
-            return review;
+        {            
+            return db.Reviews.Single(review => review.Id == id);
         }
     }
 }
