f0e18d91bd8aa02cf483a89514a057dd66dc63f9
khanishvili <khnshvl@gmail.com>
2019-03-02 01:00:06 -0500

    Created UserReviewRepository:

---
+++ ReviewSite/Repositories/UserReviewRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReviewSite.Models;
+namespace ReviewSite.Repositories
+{
+    public class UserReviewRepository
+    {
+        ReviewContext db;
+        public UserReviewRepository(ReviewContext db)
+        {
+            this.db = db;
+        }
+        public void CreateReview(UserReview review) //Creates UserReview
+        {
+            db.Add(review);
+            db.SaveChanges();
+        }
+        public void EditReview(UserReview review)
+        {
+            db.Update(review);
+            db.SaveChanges();
+        }
+        public void Delete(UserReview review)
+        {
+            db.Remove(review);
+            db.SaveChanges();
+        }
+    }
+}
